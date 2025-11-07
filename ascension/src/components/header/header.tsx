fb903e6f896c68e8750d02e37134afb8ee4d8b30
ImJustExisting <kaelawhelen7@gmail.com>
2025-11-07 11:30:16 +0100

    Update header.tsx

--- ascension/src/components/header/header.tsx
+++ ascension/src/components/header/header.tsx
@@ -1,3 +1,15 @@
 import React, {useState} from "react";
 import styles from "./header.module.css";
 
+const Header: React.FC = () => {
+    <header className={styles.backGround}>
+        <nav>
+            <ul>
+                <li><a className={styles.headerLink}>ABOUT</a></li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+
+    </header>
+}
