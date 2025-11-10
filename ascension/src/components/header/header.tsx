e0d42d9f06d8a88944284680835f4326e2ecec42
ImJustExisting <kaelawhelen7@gmail.com>
2025-11-10 09:52:39 +0100

    installed react and vite, also added logo

--- ascension/src/components/header/header.tsx
+++ ascension/src/components/header/header.tsx
@@ -1,15 +1,24 @@
 import React, {useState} from "react";
-import styles from "./header.module.css";
+import styles from './header.module.css';
 
 const Header: React.FC = () => {
-    <header className={styles.backGround}>
-        <nav>
-            <ul>
-                <li><a className={styles.headerLink}>ABOUT</a></li>
-                <li>About</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
+    return (
+        <header className={styles.backGround}>
+            <nav>
+                <ul>
+                    <li><a className={styles.headerLink}>ABOUT</a></li>
+                    <li><img src="assets/GapMarkers.png" alt="gap marker" /></li>
+                    <li><a className={styles.headerLink}>GAME PLAY</a></li>
+                    <li><img src="assets/GapMarkers.png" alt="gap marker" /></li>
+                    <li><img src="assets/GameLogo.png" alt="game logo" /></li>
+                    <li><img src="assets/GapMarkers.png" alt="gap marker" /></li>
+                    <li><a className={styles.headerLink}>THE TEAM</a></li>
+                    <li><img src="assets/GapMarkers.png" alt="gap marker" /></li>
+                    <li><a className={styles.headerLink}>PRE ORDER</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
 
-    </header>
-}
+export default Header;
