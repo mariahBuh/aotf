e5785fadfb2381b8d907ed4be0b740bd979973e9
ImJustExisting <kaelawhelen7@gmail.com>
2025-11-14 11:11:46 +0100

    pushed header

--- ascension/src/App.tsx
+++ ascension/src/App.tsx
@@ -4,11 +4,13 @@ import About from './components/About/about';
 import GamePlay from './components/GamePlay/gameplay';
 import './App.css'
 import './styles/fonts.css';
+import Header from './components/Header/header';
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
       <About />
       <GamePlay />
       <Footer />
